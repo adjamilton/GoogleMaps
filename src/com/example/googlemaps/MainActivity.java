3bb096fc0c44977ea5a5d763c8f863f04ca5437e
adjamilton <adjamilton@gmail.com>
2015-03-28 02:28:50 -0700

    google map

---
+++ src/com/example/googlemaps/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.googlemaps;
+
+import java.util.Map;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends Activity implements OnMapReadyCallback {
+
+    @Override
+    
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        
+
+        
+        
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(49.20371, -122.91459), 13));
+
+        // Other supported types include: MAP_TYPE_NORMAL,
+        // MAP_TYPE_TERRAIN, MAP_TYPE_HYBRID and MAP_TYPE_NONE
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        
+    }
+
+
+}
